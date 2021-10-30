5ab00b5b79fcbf64dbdb7e0415168ed1ea24a128
Denis Valdivieso <sshshaka@gmail.com>
2021-10-29 22:29:55 -0400

    Agregando estilos con emotion/styled (styled components)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div>
+    <Container>
       <Header titulo="Cotizador de Seguros" />
-    </div>
+      <ContainerForm>hi!</ContainerForm>
+    </Container>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
-const Header = ({ titulo }) => (
-  <header>
-    <h1>{titulo}</h1>
-  </header>
-);
+import styled from "@emotion/styled";
+
+const ContainerHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+const TextHeader = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: "Slabo 27px", serif;
+  text-align: center;
+`;
+
+const Header = ({ titulo }) => {
+  return (
+    <ContainerHeader>
+      <TextHeader>{titulo}</TextHeader>
+    </ContainerHeader>
+  );
+};
 
 export default Header;
